ba41952adb48a294c34c8f3a0bbc413719bf6813
Lucas Souza <161390698+LucassCruzz@users.noreply.github.com>
2025-06-18 19:29:26 -0300

    Update FuncionarioTemporario.java

--- ed03/folha_de_pagamento/folha_de_pagamento/src/FuncionarioTemporario.java
+++ ed03/folha_de_pagamento/folha_de_pagamento/src/FuncionarioTemporario.java
@@ -2,7 +2,7 @@
  * A classe FuncionarioTemporario representa um funcionário com contrato temporário.
  * Este tipo de funcionário tem um desconto fixo em seu salário base.
  *
- * @author Seu Nome
+ * @author Lucas Henrique Cruz de Souza
  * @version 1.0
  * @since 2025-05-28
  */
