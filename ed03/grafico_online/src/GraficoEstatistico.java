116fff7bb22a539988462d76575d55b93a0cecf0
Lucas Souza <161390698+LucassCruzz@users.noreply.github.com>
2025-06-18 19:30:32 -0300

    Update GraficoEstatistico.java

--- ed03/grafico_online/src/GraficoEstatistico.java
+++ ed03/grafico_online/src/GraficoEstatistico.java
@@ -2,7 +2,7 @@
  * A classe GraficoEstatistico representa um gráfico estatístico imprimível.
  * Implementa a interface Imprimivel para exibir seu tipo de gráfico e título.
  *
- * @author Seu Nome
+ * @author Lucas Henrique Cruz de Souza
  * @version 1.0
  * @since 2025-05-28
  */
